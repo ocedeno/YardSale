2e9577896097ffcbb562561a528c9d5bf8cfc2dc
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-03-21 09:12:39 -0400

    docs: Added viewController classes to project to manage the Login Views

---
+++ Yard Sale/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Yard Sale
+//
+//  Created by Octavio Cedeno on 3/21/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Yard Sale/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  Yard Sale
+//
+//  Created by Octavio Cedeno on 3/21/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
