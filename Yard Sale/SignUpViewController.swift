5c39c1afae4a6293a1cd1099e1433c831da5bfcb
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-03-21 11:29:07 -0400

    docs: Added forwardGeocoding() and adjusted the Else Event of getCoordinates()
    The forwardGeocoding method will use the manual address provided by the user to locate a placemark near the address and use those coordinates as the users location.

--- Yard Sale/SignUpViewController.swift
+++ Yard Sale/SignUpViewController.swift
@@ -46,13 +46,12 @@ class SignUpViewController: UIViewController, CLLocationManagerDelegate
             addressUISetup(hidden: true)
             locationManager.requestAlwaysAuthorization()
             locationManager.requestWhenInUseAuthorization()
+            
             if CLLocationManager.locationServicesEnabled()
             {
                 locationManager.delegate = self
                 locationManager.desiredAccuracy = kCLLocationAccuracyBest
                 locationManager.startUpdatingLocation()
-                hasLocation = true
-                //add coordinates to the Database
                 //getCoordinates()
             }else{
                 addressUISetup(hidden: false)
@@ -84,11 +83,21 @@ class SignUpViewController: UIViewController, CLLocationManagerDelegate
                 let lat = userLocation.coordinate.latitude
                 let lon = userLocation.coordinate.longitude
                 print("Lat: \(lat) and Lon:\(lon)")
+                //add coordinates to the Database
+                hasLocation = true
             }
         }else{
             if checkAddressFields()
             {
-                
+                let address = "\(addressField1.text) \(addressField2.text) \(stateField.text) \(zipCodeField.text)"
+                do
+                {
+                    let userLocation = try forwardGeocoding(address: address)
+                    //add coordinates to the Database
+                    hasLocation = true
+                } catch {
+                    hasLocation = false
+                }
             }
         }
     }
@@ -128,9 +137,48 @@ class SignUpViewController: UIViewController, CLLocationManagerDelegate
         return true
     }
     
+    func forwardGeocoding(address: String) -> CLLocation
+    {
+        var clLocation: CLLocation?
+        CLGeocoder().geocodeAddressString(address, completionHandler: { (placemarks, error) in
+            
+            guard error == nil else
+            {
+                DispatchQueue.main.async
+                {
+                    let alertController = UIAlertController(title: "Incorrect Address", message: "Address information provided did not return a location. Try again.", preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "Dismiss", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+                
+                return
+            }
+            
+            if (placemarks?.count)! > 0
+            {
+                let placemark = placemarks?[0]
+                let location = placemark?.location
+                clLocation = location!
+                let coordinate = location?.coordinate
+                print("\nlat: \(coordinate!.latitude), long: \(coordinate!.longitude)")
+                
+                if (placemark?.areasOfInterest?.count)! > 0
+                {
+                    let areaOfInterest = placemark!.areasOfInterest![0]
+                    print("Areas of Interest: \(areaOfInterest)")
+                }else
+                {
+                    print("No area of interest found.")
+                }
+            }
+        })
+        
+        return clLocation!
+    }
+    
     @IBAction func createAccount()
     {
-        //getCoordinates()
         if (hasLocation!)
         {
             
