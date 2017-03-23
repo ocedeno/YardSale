8a1fb2b0e93d71cbaf42e5501be6cb7a3e997dbc
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-03-23 12:08:31 -0400

    docs: Add AlertController for signup method
    Add a UIAlertController that will get the users name, email, and password. Save button will pass information to Firebase.

--- Yard Sale/LoginViewController.swift
+++ Yard Sale/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class LoginViewController: UIViewController
 {
@@ -20,6 +22,42 @@ class LoginViewController: UIViewController
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signUpAction()
+    {
+        let alert = UIAlertController(title: "Register",
+                                      message: "Please enter your information below to create a new account.",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default)
+                                      {action in
+            let userField = alert.textFields![0]
+            let emailField = alert.textFields![1]
+            let passwordField = alert.textFields![2]
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        
+        alert.addTextField { (textUser) in
+            textUser.placeholder = "Enter your first name"
+        }
+        
+        alert.addTextField { textEmail in
+            textEmail.placeholder = "Enter your email"
+        }
+        
+        alert.addTextField { textPassword in
+            textPassword.isSecureTextEntry = true
+            textPassword.placeholder = "Enter your password"
+        }
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func loginAction()
