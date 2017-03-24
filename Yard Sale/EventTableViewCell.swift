2f3f2f643a9f7c90d2b9a4fa1b584709fc1ad5b1
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-03-24 14:31:24 -0400

    docs: Add EventTableViewCell swift file to the Project

---
+++ Yard Sale/EventTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventTableViewCell.swift
+//  Yard Sale
+//
+//  Created by Octavio Cedeno on 3/24/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class EventTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
