b186bd60e6d98fbfb8bd087bc4f02f40f3422f24
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-03-24 14:03:44 -0400

    docs: Add Current Location to MapView on MainVC
    Add the request authorization to the plist. Ask user for permission then get coordinates from current location and add it to the map view on the MainVC View.

--- Yard Sale/MainViewController.swift
+++ Yard Sale/MainViewController.swift
@@ -9,11 +9,15 @@
 import UIKit
 import Firebase
 import MapKit
+import CoreLocation
 
-class MainViewController: BaseViewController, MKMapViewDelegate {
+class MainViewController: BaseViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var eventTableView: UITableView!
+    
+    var locationManager: CLLocationManager!
+    let utilityClass = Utiliy()
     
     override func viewDidLoad()
     {
@@ -27,11 +31,44 @@ class MainViewController: BaseViewController, MKMapViewDelegate {
     {
         super.viewWillAppear(true)
         
+        determineCurrentLocation()
     }
     
     @IBAction func createEvent(_ sender: UIBarButtonItem)
     {
         openViewControllerBasedOnIdentifier("DetailVC")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])
+    {
+        let currentLocation: CLLocation = locations[0] as CLLocation
+        manager.stopUpdatingLocation()
+        let lon = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+        let center = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func determineCurrentLocation()
+    {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        locationManager.activityType = .automotiveNavigation
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled()
+        {
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        utilityClass.errorAlert(title: "Map Error", message: error.localizedDescription, cancelTitle: "Dismiss", view: self)
     }
 }
 
