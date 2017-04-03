b21ff5bb687c364ddfb1908d217b51859a365b49
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-04-03 15:27:48 -0400

    docs: Add UserProfileVC

---
+++ Yard Sale/UserProfileViewController.swift
@@ -0,0 +1,14 @@
+//
+//  UserProfileViewController.swift
+//  Yard Sale
+//
+//  Created by Octavio Cedeno on 4/3/17.
+//  Copyright © 2017 Cedeno Enterprises, Inc. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController
+{
+    
+}
