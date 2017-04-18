a5b4244b5b4000f39dd901dc91e18571ff7af87b
Octavio Cedeno <ocedeno@users.noreply.github.com>
2017-04-18 07:57:23 -0400

    docs: Add internet check method into utlity class
    Call this method during the initial startup of the mainVC and right before a user saves a new event.

--- Yard Sale/Utility.swift
+++ Yard Sale/Utility.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SystemConfiguration
 
 struct Utility
 {
@@ -30,6 +31,27 @@ struct Utility
         imageView.contentMode = .scaleAspectFill
         view.addSubview(imageView)
         view.sendSubview(toBack: imageView)
+    }
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
 }
 
